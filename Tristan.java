147daffec7639487e93786a1ffbb9d7e52db94bd
kirill-lunin <160884349+kirill-lunin@users.noreply.github.com>
2024-02-22 11:24:05 -0600

    updated tristan class
    added methods

--- Tristan.java
+++ Tristan.java
@@ -1,12 +1,18 @@
 public class Tristan extends Vazquez {
 	private String firstName;
+	private double stupidityLevel;
 
 	public Tristan(){
 		firstName = "tristan";
+		stupidityLevel = 0.0;
 	}
 
 	public String getFirstName() {
 		return firstName;
+	}
+
+	public void setName(String name){
+		firstName = name;
 	}
 
 	public String getSurname(){
@@ -18,6 +24,16 @@ public class Tristan extends Vazquez {
 	}
 
 	public void bite(){
-		System.out.println("Tristan says *bites*");
+		System.out.println(firstName +" says: *bites*");
+	}
+
+	public void hello(){
+		System.out.println(firstName + " says: hi my name is " + firstName + " and i am part of the vazquez class");
+	}
+
+	public void argue(Tristan otherTristan){
+		System.out.println(firstName +" says *SHUT UP " + otherTristan.getFirstName() + "*");
+		stupidityLevel++;
+		System.out.println(firstName + "'s stupidity level increased! It is now " + stupidityLevel);
 	}
 }
