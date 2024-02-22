c5c877ad1e555c738dbc38c6e013936d6aab2cc2
123dprinter <73493250+123dprinted@users.noreply.github.com>
2024-02-22 09:24:49 -0600

    added files
    all the tristan code

---
+++ Vazquez.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+
+public class Vazquez {
+	protected String lastName;
+	protected double iq;
+
+	public Vazquez(){
+		lastName = "viscus";
+		iq = Math.random();
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public double getIntelligence(){
+		return iq * 5.0;
+	}
+}
